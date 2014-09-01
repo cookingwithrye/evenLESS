629aec71819f35deee49be19af397e3296783dd7
Ryan Weir <ryweir@gmail.com>
2014-09-01 16:07:06 -0400

    removed hardcoded LESS template from html

--- js/app.js
+++ js/app.js
@@ -1,32 +1,46 @@
-var lessOutput, rawLess,
-    parser = new(less.Parser)(),
-    CssCompile = {
-      $style: $('textarea#output'),
-      typingTimer: null,
-      init: function() {
-        var _this = CssCompile;
-        rawLess = $('textarea#source').val();
-        rawLess = "//Generated Variables" + '\n' + rawLess;
-        rawLess = "@color1:" + $('#color1').val() + ';\n' + rawLess;
-        rawLess = "@color2:" + $('#color2').val() + ';\n' + rawLess;
-        if (rawLess) _this.compile(rawLess);
-      },
-      compile: function(raw) {
-        var _this = CssCompile;
-        parser.parse(raw, function(err, css){
-          if (err) {
-            lessOutput = err;
-            // Do some error handling
-			// or not...
-          }
-          if (css) {
-            lessOutput = css.toCSS({ compress: false }); //TODO: control this with a checkbox on the input form
-            _this.$style.text(lessOutput);
-          }
-        });
-      }
-    };
+var rawLess = null;
 
-$('#generate').click(function() {
-  CssCompile.init();
+$(function() {
+	$.ajax({
+		url : './less_template.less',
+		async: false,
+		success : function (lessFile) {
+			rawLess = lessFile;
+		}
+	});
+});
+
+$(function() {
+	var lessOutput,
+		parser = new(less.Parser)(),
+		CssCompile = {
+		  $style: $('textarea#output'),
+		  typingTimer: null,
+		  init: function() {
+			if (rawLess !== null && typeof rawLess != 'undefined') {
+				rawLess = "//Generated Variables" + '\n' + rawLess;
+				rawLess = "@color1:" + $('#color1').val() + ';\n' + rawLess;
+				rawLess = "@color2:" + $('#color2').val() + ';\n' + rawLess;
+				CssCompile.compile(rawLess);
+			}
+		  },
+		  compile: function(raw) {
+			var _this = CssCompile;
+			parser.parse(raw, function(err, css){
+			  if (err) {
+				lessOutput = err;
+				// Do some error handling
+				// or not...
+			  }
+			  if (css) {
+				lessOutput = css.toCSS({ compress: false }); //TODO: control this with a checkbox on the input form
+				_this.$style.text(lessOutput);
+			  }
+			});
+		  }
+		};
+
+	$('#generate').click(function() {
+	  CssCompile.init();
+	});
 });
