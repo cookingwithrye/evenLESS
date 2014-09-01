6a1ea0cb86457a0fc0c667ff4d4078f882cc9e76
Ryan Weir <ryweir@gmail.com>
2014-09-01 16:12:06 -0400

    cleanup unused variable

--- js/app.js
+++ js/app.js
@@ -19,22 +19,23 @@ $(function() {
 		  init: function() {
 			if (rawLess !== null && typeof rawLess != 'undefined') {
 				rawLess = "//Generated Variables" + '\n' + rawLess;
-				rawLess = "@color1:" + $('#color1').val() + ';\n' + rawLess;
+				
+				//TODO: make this dynamically accept any number of variables
+				rawLess = "@color1:" + $('#color1').val() + ';\n' + rawLess; 
 				rawLess = "@color2:" + $('#color2').val() + ';\n' + rawLess;
+				
 				CssCompile.compile(rawLess);
 			}
 		  },
 		  compile: function(raw) {
-			var _this = CssCompile;
 			parser.parse(raw, function(err, css){
 			  if (err) {
 				lessOutput = err;
-				// Do some error handling
-				// or not...
+				// Do some error handling...or not
 			  }
 			  if (css) {
 				lessOutput = css.toCSS({ compress: false }); //TODO: control this with a checkbox on the input form
-				_this.$style.text(lessOutput);
+				CssCompile.$style.text(lessOutput);
 			  }
 			});
 		  }
